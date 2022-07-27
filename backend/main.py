c4d67aec5f98b62b771083643c766808bae66437
anirbanmajumder0 <81350357+anirbanmajumder0@users.noreply.github.com>
2022-07-27 09:58:23 +0530

    Passing flake8

--- backend/main.py
+++ backend/main.py
@@ -1,22 +1,30 @@
 from fastapi import FastAPI, WebSocket
-import time
 
 app = FastAPI()
 
+
 @app.websocket_route("/ws")
 async def websocket_endpoint(websocket: WebSocket):
+    """Websocket server endpoint
+
+    This function is called when a websocket connection is made.
+    and it is used to send and recive data to the client.
+    """
     await websocket.accept()
     while True:
-        
-        receive_data = await websocket.receive_json()
-        #Todo: take this data and calculate where more food will spawn
-        #and which direction would it move
-        print(receive_data)
 
-        #Todo: send back the food location and direction where the food will move 
+        # Recive data from client
+        receive_data = await websocket.receive_json()
+        print(receive_data)
+        # Todo: take this data and calculate where more food will spawn
+        # and which direction would it move
+
+        # sending dummy data to the client
         data = {
             "type": "move"
         }
         await websocket.send_json(data)
+        # Todo: send back the food location and direction where the food will move
 
-#try to do it functionally 
+
+# Try to do it functionally and efficiently (cpu budget: 100ms+network budget: 40ms)
